61982685fba65023b45f61008f52026c0acb64d1
Marcus <marcus@r38.se>
2025-01-01 09:37:50 +0100

    reducing disk usage

--- flake.nix
+++ flake.nix
@@ -18,6 +18,7 @@
 	      specialArgs = { inherit inputs; };
 	      modules = [
 	        ./configuration.nix
+          ./clean.nix
           home-manager.nixosModules.home-manager {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
