45118b7e2ab82b680999bdffa15b1e225f286f50
marcus-38 <marcus@r38.se>
2025-01-07 18:02:45 +0100

    Auto generated commit

--- configuration.nix
+++ configuration.nix
@@ -63,7 +63,7 @@
   services.printing.enable = true;
 
   # Enable sound with pipewire.
-  service.pulseaudio.enable = false;
+  hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
