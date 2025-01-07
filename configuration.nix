143d65f2bd79de67eeec45b84b9aeb2a0b457648
marcus-38 <marcus@r38.se>
2025-01-07 18:00:56 +0100

    installed just

--- configuration.nix
+++ configuration.nix
@@ -116,6 +116,7 @@
   environment.systemPackages = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     git
+    just
     gh
     wget
   ];
